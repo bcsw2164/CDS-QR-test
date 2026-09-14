dde4413bda67aa08f25884bd8347c3fff4bca5fb
bcsw2164 <126849531+bcsw2164@users.noreply.github.com>
2026-09-14 17:13:37 +0900

    이미지 추가, 폴더 정리, 인터페이스 정리

--- behind/script.js
+++ behind/script.js
@@ -4,11 +4,11 @@
    프로젝트에 실제로 참여한 사람들의 사진을 그리드로 보여준다.
 
    사진 추가 방법:
-     images/01.jpg, 02.jpg, ... 형식으로 번호를 이어 저장하고
+     images/onsite/01.jpg, 02.jpg, ... 형식으로 번호를 이어 저장하고
      아래 PHOTO_COUNT를 사진 장수에 맞게 올리면 된다.
    ============================================================ */
 
-const PHOTO_COUNT = 17;
+const PHOTO_COUNT = 20;
 
 function pad2(n) {
   return String(n).padStart(2, '0');
@@ -23,7 +23,7 @@ function buildPhotoGrid() {
     cell.className = 'photo-cell';
 
     const img = document.createElement('img');
-    img.src = `images/${pad2(i)}.jpg`;
+    img.src = `images/onsite/${pad2(i)}.jpg`;
     img.alt = `참여 사진 ${i}`;
     img.loading = 'lazy';
 
